a1648e9685a289e2e512b42a2d35228ed8c40257
Gabriel Gutierrez <gabri.gutiz@gmail.com>
2024-04-30 23:50:16 -0300

    feat: add configs to UI and usability and lsp keymaps

--- lua/gutierrez/init.lua
+++ lua/gutierrez/init.lua
@@ -2,9 +2,30 @@
 require("gutierrez.remap")
 require("gutierrez.lazy_init")
 
-vim.g.netrw_browse_split = 0
-vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 25
+local g = vim.g
+local opt = vim.opt
 
-vim.wo.number = true
+----------------------------------------------------------
+-- Folder navigation style
+----------------------------------------------------------
+g.netrw_browse_split = 0
+g.netrw_banner = 0
+g.netrw_winsize = 25
+
+----------------------------------------------------------
+-- UI
+----------------------------------------------------------
+opt.showmatch = true     -- highlight matching parenthesis
+opt.colorcolumn = "90"   -- column maker
+opt.ignorecase = true    -- ignore case letters for seach
+
 vim.wo.relativenumber = true
+
+----------------------------------------------------------
+-- Tabs and ident
+----------------------------------------------------------
+opt.expandtab = true     -- use spaces instead of tabs
+opt.shiftwidth = 4       -- shift 4 spaces when tab
+opt.tabstop = 4          -- tab size
+opt.smartindent = true   -- autoindent new lines
+
