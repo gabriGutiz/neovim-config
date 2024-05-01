bd49cec6fec824bdbb22ba1be1ee6e5fc676bf7d
Gabriel Gutierrez <gabri.gutiz@gmail.com>
2024-04-30 23:13:35 -0300

    feat: add exit from insert mode, force quit and save quit

--- lua/gutierrez/remap.lua
+++ lua/gutierrez/remap.lua
@@ -2,6 +2,10 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>p", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>w", vim.cmd.w)
-vim.keymap.set("n", "<leader>q", vim.cmd.q)
+vim.keymap.set("n", "<leader>w", ":w<CR>")
+vim.keymap.set("n", "<leader>sq", ":wq<CR>")
+vim.keymap.set("n", "<leader>q", ":q<CR>")
+vim.keymap.set("n", "<leader>fq", ":qa!<CR>")
+
+vim.keymap.set("i", "kk", "<C-c>")
 
