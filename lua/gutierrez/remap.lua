d723ddb197b208ced46d8a02e25afc7ce1522931
Gabriel Gutierrez <gabri.gutiz@gmail.com>
2024-05-06 12:33:36 -0300

    feat: add go to definition remaps

--- lua/gutierrez/remap.lua
+++ lua/gutierrez/remap.lua
@@ -7,5 +7,8 @@ vim.keymap.set("n", "<leader>sq", ":wq<CR>")
 vim.keymap.set("n", "<leader>q", ":q<CR>")
 vim.keymap.set("n", "<leader>fq", ":qa!<CR>")
 
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+
 vim.keymap.set("i", "kk", "<C-c>")
 
