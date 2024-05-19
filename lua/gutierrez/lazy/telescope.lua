f0fec86756d62bb77d3686ad4379aed4a0f9f362
Gabriel Gutierrez <gabri.gutiz@gmail.com>
2024-05-19 10:55:45 -0300

    feat: add logic to <leader>ff on telescope to open find_files if not in git, otherwise git_files

--- lua/gutierrez/lazy/telescope.lua
+++ lua/gutierrez/lazy/telescope.lua
@@ -6,11 +6,25 @@ return {
     },
 
     config = function()
-        require('telescope').setup({})
+        require('telescope').setup({
+            defaults = {
+                path_display = {
+                    "filename_first",
+                },
+            },
+        })
 
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>ff', function()
+            local git_dir = vim.fn.finddir('.git', vim.fn.getcwd() .. ";")
+
+            if git_dir ~= "" then
+                builtin.git_files()
+            else
+                builtin.find_files()
+            end
+        end, {})
+        vim.keymap.set('n', '<leader>fa', builtin.find_files, {})
         vim.keymap.set('n', '<leader>fs', function()
             builtin.grep_string({ search = vim.fn.input("grep > ") })
         end)
