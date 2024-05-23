67bad71c7962d1bb895c0ee76bd073485ad4debd
Gabriel Gutierrez <gabri.gutiz@gmail.com>
2024-05-23 10:32:53 -0300

    feat: add live_grep as <leader>fg on telesecope

--- lua/gutierrez/lazy/telescope.lua
+++ lua/gutierrez/lazy/telescope.lua
@@ -25,6 +25,7 @@ return {
             end
         end, {})
         vim.keymap.set('n', '<leader>fa', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>fs', function()
             builtin.grep_string({ search = vim.fn.input("grep > ") })
         end)
